1f4408de643981bc9b6a4654319b33fcc6392a28
Miracle Innocent <MiracleIyiagwu@gmail.com>
2023-01-10 14:36:03 +0100

    Initial Commit

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _context.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+    }
+}
+
+
